a009fe8bddaedc02efd0def299635b6b5c876187
Moghal Saif <moghalsaif@outlook.com>
2026-08-04 23:22:28 +0530

    Revert "feat: add India weight loss challenge website"
    This reverts commit 5b92fa4fb4a87267f5d1a8d0be629d07e88e08d6.

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.indiaweightlosschallenge.in",
-        pathname: "/images/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.indiaweightlosschallenge.in",
-        pathname: "/app/banners/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.indiaweightlosschallenge.in",
-        pathname: "/app/tips/**",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        pathname: "/wikipedia/commons/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
